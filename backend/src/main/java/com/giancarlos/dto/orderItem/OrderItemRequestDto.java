3a8ad0215ae71f582761c06d69c3d06202778753
gmarte10 <giancarlosmarte10@gmail.com>
2024-12-01 21:08:46 -0500

    feat(dto/orderItem): create new and refined orderItem dto, request and response

---
+++ backend/src/main/java/com/giancarlos/dto/orderItem/OrderItemRequestDto.java
@@ -0,0 +1,19 @@
+package com.giancarlos.dto.orderItem;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class OrderItemRequestDto {
+    private Long orderId;
+    private Long productId;
+    private Integer quantity;
+    private BigDecimal price;
+}
---
+++ backend/src/main/java/com/giancarlos/dto/orderItem/OrderItemResponseDto.java
@@ -0,0 +1,20 @@
+package com.giancarlos.dto.orderItem;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class OrderItemResponseDto {
+    private Long id;
+    private Long orderId;
+    private ProductResponseDto product;
+    private Integer quantity;
+    private BigDecimal price;
+}
