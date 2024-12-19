bb368d305afc2cbed56b5a0c81c04e6ee4f5932a
gmarte10 <giancarlosmarte10@gmail.com>
2024-12-19 17:27:24 -0500

    save dummy sql

--- backend/db/dummy.sql
+++ backend/db/dummy.sql
@@ -4,31 +4,59 @@ INSERT INTO products (name, category, description, price, image_url)
 VALUES
 	(
 	'iphone 15',
-	'tech',
+	'technology',
 	'The iPhone 15 features USB-C, A16 chip, 48MP camera, and iOS 17, with improved performance.',
-	1000,
+	899.99,
 	'iphone15.jpg'
 	),
 	(
-	'cetics hoodie',
+	'hoodie',
 	'clothing',
 	'A cozy hoodie with a soft interior, adjustable hood, and a front pocket for comfort.',
-	50,
-	'celticshoodie.jpg'
+	50.50,
+	'hoodie.jpg'
 	),
 	(
 	'crispy chicken sandwich',
 	'food',
 	'A premium handmade delicious crispy chicken sandwich.',
-	10,
+	10.99,
 	'ccsandwich.jpg'
 	),
 	(
-	'sony m5 headphones',
-	'tech',
-	'Comftorable high quality headphones by sony.',
-	300,
-	'sonym5.jpg'
+	'headphones',
+	'technology',
+	'Comftorable high quality headphones.',
+	398,
+	'headphones.jpg'
+	),
+	(
+	'apple',
+	'food',
+	'Organic healthy sweet fruit.',
+	2.99,
+	'apple.jpg'
+	),
+	(
+	'jeans',
+	'clothing',
+	'Premium durable pair of jeans.',
+	36.99,
+	'jeans.jpg'
+	),
+	(
+	'the matrix',
+	'movie',
+	'HD hard copy of the famous matrix movie.',
+	15.99,
+	'matrix.jpg'
+	),
+	(
+	'table',
+	'furniture',
+	'Well built hand made table.',
+	235.99,
+	'table.jpg'
 	);
 
 
